d404ee39bda232c6e010489dbea1dc67f9fd627a
Dagorik <dagorik@gmail.com>
2018-08-06 20:38:51 -0500

    add conection to mongodb

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ import express from 'express'
 import parser from 'body-parser'
 import cors from 'cors';
 import mongoose from 'mongoose';
-import User from './src/models/user';
 
 
 const app = express();
 const PORT = process.env.PORT || 3000
+const mongoURI = process.env.MONGODB_URI || "mongodb://prueba:Test1234567890@ds259079.mlab.com:59079/cinta-roja"
 
-mongoose.connect('mongodb://admin:admin1234@ds127851.mlab.com:27851/netflix-b21',{ useNewUrlParser: true })
+mongoose.connect(mongoURI,{useNewUrlParser: true});
 const db = mongoose.connection;
-db.on('error',() => console.log("Error al conectar a la BD"))
-    .once('open',() => console.log("Conectado a la BD!!"))
+db.on('error',() => console.log('Error al conectar a la BD'))
+    .once('open',() => console.log('Conectado a la BD'))
 
 app.use(parser.json());
 app.use(cors());
@@ -19,6 +19,8 @@ app.use(cors());
 app.get('/',(req,res)=>{
     res.send('Server adss')
 });
+
+
 
 app.post('/signup',(req,res) => {
     let user = req.body

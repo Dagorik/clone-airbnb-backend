f8c840f75c662ded76b92779f2b5d5785b5b7667
Dagorik <dagorik@gmail.com>
2018-08-15 00:14:14 -0500

    error de test

--- src/tests/helpers.js
+++ src/tests/helpers.js
@@ -1,55 +1,71 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-process.env.NODE_ENV = 'test'
+
+const { ObjectId } = mongoose.Types;
+
+process.env.NODE_ENV = 'test';
 
 const config = {
-    db:{
-        test:'mongodb://prueba:prueba123@ds221242.mlab.com:21242/testing'
-    },
-    connection:null
-}
+  db: {
+    test: 'mongodb://prueba:prueba123@ds221242.mlab.com:21242/testing',
+  },
+  connection: null,
+};
 
-function connect(){
-    return new Promise((resolve,reject) => {
+function connect() {
+  return new Promise((resolve, reject) => {
+    if (config.connection) {
+      return resolve();
+    }
 
-        const options = {
-            server:{
-                auto_reconnect:true,
-                reconnectTries: Number.MAX_VALUE,
-                reconnectInterval:1000
-            }
-        };
-        mongoose.connect(config.db.test,options);
-        config.connection = mongoose.connection;
-        config.connection
-            .once('open',resolve)
-            .on('error',(e) => {
-                console.log(e)
-                reject()
-            })
-            
-    });
-}
+    const mongoUri = 'mongodb://prueba:prueba123@ds221242.mlab.com:21242/testing';
 
+    mongoose.Promise = Promise;
 
-function clearDatabase(){
-    return new Promise((resolve,reject) => {
-        let cont = 0
-        let max = Object.keys(mongoose.connection.collection).length;
-        
-        for(const i in mongoose.connection.collection){
-            mongoose.connection.collection[i].remove(function (){
-                cont++;
-                if (cont >= max){
-                    resolve();
-                }
-            });
+    const options = {
+      server: {
+        auto_reconnect: true,
+        reconnectTries: Number.MAX_VALUE,
+        reconnectInterval: 1000,
+      },
+      useNewUrlParser: true
+    };
+
+    mongoose.connect(mongoUri, options);
+
+    config.connection = mongoose.connection;
+
+    config.connection
+      .once('open', resolve)
+      .on('error', (e) => {
+        if (e.message.code === 'ETIMEDOUT') {
+          console.log(e);
+
+          mongoose.connect(mongoUri, options);
         }
-    });
+
+        console.log(e);
+        reject(e);
+      });
+  });
 }
 
+function clearDatabase() {
+    return new Promise(resolve => {
+      let cont = 0;
+      let max = Object.keys(mongoose.connection.collections).length;
+      for (const i in mongoose.connection.collections) {
+        mongoose.connection.collections[i].remove(function() {
+          cont++;
+          if(cont >= max) {
+            resolve();
+          }
+        });
+      }
+    });
+  }
 
-export async function setUpTest(){
-    await connect()
-    await clearDatabase()
+export async function setupTest() {
+  await connect();
+   //await clearDatabase();
 }
--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -1,5 +1,71 @@
+import {graphql} from 'graphql'
+import {setupTest} from './helpers'
+import schema from '../graphql'
+import User from '../models/users'
+import generarToken from '../resolvers/createToken'
 
-import {setUpTest} from './helpers'
+
+describe('Prueba de User',async  () => {
+    
+    beforeEach(async () => await setupTest());
+    
+    it('Probando query sin token deberia ser null',async () => {
+        //language=GraphQL
+        const query = `
+        query {
+            me{
+                _id
+                 nombre
+                username
+            }
+        }
+        `;
+        console.log('a,msdnam,nd')
+        const result = await graphql(schema,query,{},{})
+            const {data} = result;
+            expect(data.me).toBe(null)
+        
+    })
+
+//     it('Probando query de me que deberia ser un usuario',async () => {
+//         console.log('Iniciando el test 2')
+//         //language=GraphQL
+//         const query = `
+      
+//         query{
+//             me{
+//                 _id,
+//                 name
+//             }
+//         }
+    
+//     `;
+//     console.log('Paso const query')
+
+//     const user = new User({
+//         "nombre":"Juanito 2",
+//         "apellidos":"Perex 2",
+//         "correo":"dagorik4@gmai.com",
+//         "fecha_nacimiento":"1994-04-12",
+//         "password":"elpassword2",
+//         "username":"ad",
+//         "genero":"M",
+//         "pais":"MX",
+//         "ubicacion":"EN MI CASITA",
+//         "rol":"USER"
+//     })
+//     console.log('creo el usuario',user)
+//     const userSaved = await  user.save();
+//     console.log('ya lo guardo')
+//     const context =  {user:userSaved}
+//     console.log('A punto de pasarle el schema')
+//     const result = await graphql(schema,query,{},context);
+//     console.log('Ya existe resutl')
+//     const {data} = result;
+//     expect(data).toBe(undefined);
+//   })  
+})
+
 
 it('Suma de 2 mas 2', () => {
     expect(2+2).toBe(4)
